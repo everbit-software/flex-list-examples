d941470e6b21817a7fb9c7b1f132d12c95db8b62
Harry <harryh@lyke.co.uk>
2023-06-20 22:16:41 +0100

    List examples with 3 types of renderers

---
+++ src/ts/dummy-api.ts
@@ -0,0 +1,24 @@
+import DynamicList from "../../dynamic-list/dynamic-list"
+
+export interface Product {
+    id: number
+    title: string
+    description: string
+    price: number
+    discountPercentage: number
+    rating: number
+    stock: number
+    brand: string
+    category: string
+    thumbnail: string
+    images: string[]
+}
+
+export async function getRows(page = 1, limit = 20, query?: string) {
+    let response = await fetch(`https://dummyjson.com/products${query ? '/search' : ''}?limit=${limit}&skip=${(page - 1) * limit}${query ? '&q=' + query : ''}`);
+    let { products, total } = await response.json();
+
+    // list.pagination.max = total / limit;
+
+    return products as Product[];
+}
